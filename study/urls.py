839d745feb316ce33cb6a4e5fac25cc306b560c9
bardnia <69304793+bardnia@users.noreply.github.com>
2023-12-16 22:00:02 +0900

    주석 제거

--- study/urls.py
+++ study/urls.py
@@ -8,9 +8,4 @@ urlpatterns = [
     path('<int:pk>/', views.StudygroupRetrieveAPIView.as_view(), name='studydetail'),
     path('<int:pk>/update/', views.StudygroupUpdateAPIView.as_view(), name='studyupdate'),
     path('<int:pk>/delete/', views.StudygroupDestroyAPIView.as_view(), name='studydelete'),
-    # path('comment/', views.StudygroupCreateView.as_view(), name='study'),
-    # path('comment/<int:pk>/', views.~~~, name='study'),
-    # path('member/', views.~~~, name='study'),
-    # path('change/master/', views.~~~, name='study'),
-    # path('join/', views.~~~, name='study'),
 ]
