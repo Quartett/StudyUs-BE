1c489fc9fa7ee1e1acdc736a2a1e8367ee24bf0f
eric <huny3410@gmail.com>
2023-12-17 16:18:32 +0900

    :bug: fix: 채팅 모델에서 스터디 -> 스터디그룹으로 모델 변경 반영

--- chat/models.py
+++ chat/models.py
@@ -5,11 +5,11 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class ChatRoom(models.Model):
-    study = models.OneToOneField(StudyGroup, on_delete=models.CASCADE, related_name='chat_room')
+    study_group = models.OneToOneField(StudyGroup, on_delete=models.CASCADE, related_name='chat_room')
 
 
 class Chat(models.Model):
     chat_room = models.ForeignKey(ChatRoom, on_delete=models.CASCADE, related_name='chats')
-    user = models.ForeignKey(StudyUsUser, on_delete=models.CASCADE, related_name='users')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='users')
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
