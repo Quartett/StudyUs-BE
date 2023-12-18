984db2cb310dd8806d783a1971cb9cece5f92073
eric <huny3410@gmail.com>
2023-12-18 20:29:56 +0900

    :fire: remove: asgi 코드 롤백

--- studyus/asgi.py
+++ studyus/asgi.py
@@ -3,7 +3,6 @@ from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from .middlewares import JWTAuthMiddleware
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "studyus.settings")
 django.setup()
@@ -14,6 +13,6 @@ django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
     "websocket": AllowedHostsOriginValidator(
-        JWTAuthMiddleware(URLRouter(websocket_urlpatterns))
+        AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
     )
 })
