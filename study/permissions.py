03ca20a74eeeac10ffea4a2cab57efa3de6a3af2
su2minig <mini72667@gmail.com>
2023-12-20 16:51:26 +0900

    :sparkles: feat: 그룹장 권한 기능

--- study/permissions.py
+++ study/permissions.py
@@ -2,15 +2,12 @@ from rest_framework import permissions
 from django.shortcuts import get_object_or_404
 from study.models import StudyGroup, StudyMember
 
-SAFE_METHODS = ['GET', 'HEAD', 'OPTIONS']
-
 class MemberOnly(permissions.BasePermission):
     """
     해당 스터디 그룹의 멤버에게만 접근을 허용하는 권한 클래스
     """
 
     def has_permission(self, request, view):
-        # URL에서 study_group_id를 추출
         study_group_id = view.kwargs.get('study_group_id')
 
         # study_group_id가 없다면 False 반환
@@ -19,9 +16,24 @@ class MemberOnly(permissions.BasePermission):
 
         # StudyGroup 객체를 가져옴, 존재하지 않으면 404 에러
         study_group = get_object_or_404(StudyGroup, id=study_group_id)
+        print(StudyMember.objects.filter(study_group=study_group, user=request.user, role=1))
 
         # 요청을 보낸 사용자가 해당 스터디 그룹의 멤버인지 확인
-        return StudyMember.objects.filter(study_group=study_group, user=request.user).exists()
+        if request.method in permissions.SAFE_METHODS:
+            return StudyMember.objects.filter(study_group=study_group, user=request.user).exists()
+
+    def has_object_permission(self, request, view, obj):
+        study_group_id = view.kwargs.get('study_group_id')
+
+        if not study_group_id:
+            return False
+
+        study_group = get_object_or_404(StudyGroup, id=study_group_id)
+
+        if request.method in permissions.SAFE_METHODS:
+            return StudyMember.objects.filter(study_group=study_group, user=request.user).exists()
+        print(StudyMember.objects.filter(study_group=study_group, user=request.user, role=1))
+        return StudyMember.objects.filter(study_group=study_group, user=request.user).get().role == 1
 
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
@@ -36,7 +48,3 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         
         # 요청자(request.user)가 객체(Studygroup)의 user와 동일한지 확인
         return obj.author == request.user
-        
-class IsAuthenticated(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return request.user and request.user.is_authenticated  # 유저가 있고, 로그인 된 유저만 허용
--- study/views.py
+++ study/views.py
@@ -8,7 +8,7 @@ from django.db import transaction
 from rest_framework import views, response, status
 from drf_spectacular.utils import extend_schema
 from django.contrib.auth import get_user_model
-from .permissions import MemberOnly, IsOwnerOrReadOnly, IsAuthenticated
+from .permissions import MemberOnly, IsOwnerOrReadOnly
 
 User = get_user_model()
 
@@ -20,11 +20,12 @@ class StudygroupListAPIView(generics.ListAPIView):
 class StudygroupCreateView(generics.CreateAPIView):
     queryset = StudyGroup.objects.all()
     serializer_class = StudyGroupSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
     
     def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
+        serializer.save()
         ChatRoom.objects.create(study_group=serializer.instance)
+        StudyMember.objects.create(user=self.request.user, study_group=serializer.instance, role=1)
 
 
 class StudygroupRetrieveAPIView(generics.RetrieveAPIView):
@@ -35,14 +36,18 @@ class StudygroupRetrieveAPIView(generics.RetrieveAPIView):
 class StudygroupUpdateAPIView(generics.UpdateAPIView):
     queryset = StudyGroup.objects.all()
     serializer_class = StudyGroupSerializer
-    permission_classes = [IsOwnerOrReadOnly]
+    permission_classes = [permissions.IsAuthenticated, MemberOnly]
 
 
 class StudygroupDestroyAPIView(generics.DestroyAPIView):
     queryset = StudyGroup.objects.all()
     serializer_class = StudyGroupSerializer
+    permission_classes = [permissions.IsAuthenticated, MemberOnly]
 
 
+@extend_schema(
+        summary='댓글 작성',
+    )
 class CommentCreateView(generics.CreateAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
@@ -52,25 +57,33 @@ class CommentCreateView(generics.CreateAPIView):
         serializer.save(author=self.request.user)
 
 
+@extend_schema(
+        summary='댓글 리스트',
+    )
 class CommentListView(generics.ListAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    @extend_schema(
-        summary='댓글 리스트',
-    )
 
     def get_queryset(self):
         return self.queryset.filter(study_group_id=self.kwargs['study_group_id'])
 
 
+@extend_schema(
+    summary='댓글 업데이트'
+)
 class CommentUpdateView(generics.UpdateAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
 
 
+@extend_schema(
+    summary='댓글 삭제'
+)
 class CommentDestroyView(generics.DestroyAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
 
 
 class JoinMemberView(views.APIView):
@@ -127,20 +140,20 @@ class MemberDeleteView(views.APIView):
 
 
 class MemberUpdateView(views.APIView):
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated, MemberOnly]
     @extend_schema(
         summary='그룹장 위임',
         request=UpdateMemberSerializer,
     )
 
-    def put(self, request, study_group_id):
+    def patch(self, request, study_group_id):
         study_group = get_object_or_404(StudyGroup, id=study_group_id)
         request_user_member = get_object_or_404(StudyMember, study_group=study_group, user=request.user)
         target_user_id = request.data.get('user')
 
-        # 요청한 사용자가 그룹장인지 확인
-        if request_user_member.role != 1:
-            return response.Response({'message': '오직 그룹장만이 권한이 있습니다.'}, status=status.HTTP_403_FORBIDDEN)
+        # # 요청한 사용자가 그룹장인지 확인
+        # if request_user_member.role != 1:
+        #     return response.Response({'message': '오직 그룹장만이 권한이 있습니다.'}, status=status.HTTP_403_FORBIDDEN)
 
         with transaction.atomic():
             # 지정된 유저를 그룹장으로 설정
@@ -153,4 +166,4 @@ class MemberUpdateView(views.APIView):
             request_user_member.role = 0
             request_user_member.save()
 
-        return response.Response({'message': 'Group leadership transferred successfully'}, status=status.HTTP_200_OK)
+        return response.Response({'message': '그룹장 변경 성공'}, status=status.HTTP_200_OK)
