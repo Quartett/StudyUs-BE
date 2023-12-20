0efac5189a1557fe97936a72e732fe0fb884c357
eric <huny3410@gmail.com>
2023-12-20 10:17:15 +0900

    :bug: fix: 암기카드 생성 요청바디중 누락된 subject 필드 추가

--- memorycard/serializers.py
+++ memorycard/serializers.py
@@ -15,4 +15,3 @@ class MemoryCardSerailizer(serializers.ModelSerializer):
     class Meta:
         model = MemoryCard
         fields = ('id', 'subject', 'memory_question', 'memory_answer', 'bookmark', 'difficulty')
-        read_only_fields = ('subject',)
